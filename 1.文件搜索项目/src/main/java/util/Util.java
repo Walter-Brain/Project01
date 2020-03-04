a212001c6f83646db05f6247a30db9a0fff8c543
Walter-Brain <976964283@qq.com>
2020-03-04 18:53:10 +0800

    1

--- 1.文件搜索项目/src/main/java/util/Util.java
+++ 1.文件搜索项目/src/main/java/util/Util.java
@@ -4,33 +4,31 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Created with IntelliJ IDEA
- * Description:
- * User:S-
- * Date:2020/1/4-16:24
- * Version: 1.0
- **/
-
 public class Util {
-    static final String DATA_PATTERN = "yyyy-MM-dd HH:mm:ss";
-    private static DateFormat DATE_FORMAT = new SimpleDateFormat(DATA_PATTERN);
-    private static final String[] SIZE_NAME = {"B", "KB", "MB", "GB"};
 
+    public static final String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static DateFormat DATE_FORMAT = new SimpleDateFormat(DATE_PATTERN);
+    private static final String[] SIZE_NAMES = {"B", "KB", "MB", "GB"};
+
+    /**
+     * 文件大小转换为B/KB/MB/...
+     * @param size
+     * @return
+     */
     public static String parseSize(Long size) {
-
         int n = 0;
-        while (size >= 1024) {
-            size = size / 1024;
+        while(size >= 1024){
+            size=size/1024;
             n++;
-            if (n == 3) {
-                break;
-            }
         }
-        return size + SIZE_NAME[n];
+        return size+SIZE_NAMES[n];
     }
 
-
+    /**
+     * 日期解析
+     * @param lastModified
+     * @return
+     */
     public static String parseDate(Long lastModified) {
         return DATE_FORMAT.format(new Date(lastModified));
     }

c7dbeb69124dd2c0e45f4021e1b08b7162348d3d
Walter-Brain <976964283@qq.com>
2020-03-04 18:52:03 +0800

    1

--- 1.文件搜索项目/src/main/java/task/FileScanTask.java
+++ 1.文件搜索项目/src/main/java/task/FileScanTask.java
@@ -6,29 +6,32 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- * Created with IntelliJ IDEA
- * Description:
- * User:S-
- * Date:2020/1/5-9:06
- * Version: 1.0
- **/
-
 public class FileScanTask {
-    private final ExecutorService pool = Executors.newFixedThreadPool(4);
-    //            new ThreadPoolExecutor();
-    private static AtomicInteger COUNT = new AtomicInteger();
-    private static final CountDownLatch LATCH = new CountDownLatch(1);
+
+    private final ExecutorService pool
+//            = new ThreadPoolExecutor();
+                = Executors.newFixedThreadPool(4);
+
+//    private static volatile int COUNT;
+    private final AtomicInteger count = new AtomicInteger();
+
+    private final CountDownLatch latch = new CountDownLatch(1);
 
     private FileScanCallback callback;
 
-    public FileScanTask(FileScanCallback callback) {
+    public FileScanTask(FileScanCallback callback){
         this.callback = callback;
     }
 
+    /**
+     * 启动根目录的扫描任务
+     * @param root
+     */
     public void startScan(File root) {
-        COUNT.incrementAndGet();
-
+//        synchronized (this) {
+//            COUNT++;
+//        }
+        count.incrementAndGet();
         pool.execute(new Runnable() {
             @Override
             public void run() {
@@ -37,19 +40,28 @@ public class FileScanTask {
         });
     }
 
+    /**
+     * 等待所有扫描任务执行完毕
+     */
     public void waitFinish() throws InterruptedException {
+//        try {
+//            synchronized (this){
+//                this.wait();
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        // 等待
         try {
-            LATCH.await();
-        } finally {
-            //中断所有线程
-            pool.shutdown();//调用每个线程的interrupt（）中断
-//            POOL.shutdownNow();//调用每个线程的stop()关闭
+            latch.await();
+        }finally {
+            pool.shutdown();// 调用每个线程的interrupt()中断
+//            POOL.shutdownNow();// 调用每个线程的stop()关闭
         }
     }
 
-
-    private void list(File dir) {
-        if (!Thread.interrupted()) {
+    public void list(File dir){
+        if(!Thread.interrupted()) {
             try {
                 callback.execute(dir);
 //                System.out.println(dir.getPath());
@@ -57,9 +69,12 @@ public class FileScanTask {
                     File[] children = dir.listFiles();
                     if (children != null && children.length > 0) {
                         for (File child : children) {
-                            //启动子线程执行子文件夹的扫描任务
+                            // 启动子线程执行子文件夹的扫描任务
                             if (child.isDirectory()) {
-                                COUNT.incrementAndGet();
+//                            synchronized (this) {
+//                                COUNT++;
+//                            }
+                                count.incrementAndGet();
                                 pool.execute(new Runnable() {
                                     @Override
                                     public void run() {
@@ -68,30 +83,25 @@ public class FileScanTask {
                                 });
                             } else {
                                 callback.execute(child);
+//                                System.out.println(child.getPath());
                             }
                         }
                     }
                 }
             } finally {
-//            synchronized (POOL) {
+//            synchronized (this) {
 //                COUNT--;
-//                if (COUNT == 0) {
-//                    POOL.notifyAll();
-                if (COUNT.decrementAndGet() == 0) {
-                    LATCH.countDown();
+//                if(COUNT == 0){
+//                    // notify()
+//                    this.notifyAll();
+//                }
+//            }
+                // 所有线程执行完毕
+                if (count.decrementAndGet() == 0) {
+                    // 通知
+                    latch.countDown();
                 }
             }
-            System.out.println(dir.getPath());
         }
     }
-
-//    public static void main(String[] args) throws InterruptedException {
-//        FileScanTask task = new FileScanTask();
-//        task.startScan(new File("F:\\比特课件"));
-//
-//        synchronized (task) {
-//            task.wait();
-//        }
-//        System.out.println("执行完毕");
-//    }
 }
